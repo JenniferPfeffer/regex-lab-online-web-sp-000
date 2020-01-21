5baa50df3cbc4b5ea9d955623b79311885831df5
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-21 19:44:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,7 @@
 def starts_with_a_vowel?(word)
- if word.scan(/[auiou]/)
+ if word.match(/[auiou]/) == nil 
+   return false 
+ else return true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
