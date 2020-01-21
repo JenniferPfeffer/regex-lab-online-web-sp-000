f40e50549e6f7eebcbaa7fcb3f8cc3e25c97b8eb
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-21 19:57:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,9 @@
-def starts_with_a_vowel?(word)
- if word.match(/\A[auiou]/i) == nil 
-   return false
- else return true
-end
+ 
+ if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -14,12 +15,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$/) == nil 
-  return false 
-else return true 
-end
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/)
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
