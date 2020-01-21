a691a39002233fb0cf4c67408f4105dffebe26f9
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-21 19:47:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,9 +3,10 @@ def starts_with_a_vowel?(word)
    return false 
  else return true
 end
+end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+  text.scan(/\bun\w+ing/\b)
 end
 
 def words_five_letters_long(text)
